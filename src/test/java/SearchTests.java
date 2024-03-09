aa5cec4b6178459b834ebfd53d031c4e32325b98
sboiko <your_email@whatever.comm909mk@gmail.comgit config --global user.email your_email@whatever.com>
2024-03-09 21:18:18 +0200

    DemoQA.com selenide first test

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,4 +1,6 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.DayOfWeek;
@@ -6,32 +8,41 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SearchTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
     @Test
-    void fusritName() {
-
+    void firstTest() {
         LocalDate parsed = LocalDate.parse("24.02.2024", DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
         assertEquals(DayOfWeek.SATURDAY, parsed.getDayOfWeek());
     }
 
-
-
-
-
     @Test
     void successfulSearchTest() {
-
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 
     @Test
-    void name() {
+    void fillFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Stepan");
+        $("#lastName").setValue("Boiko");
+        $("#userEmail").setValue("stepan@boiko.com");
+        $("#genterWrapper").$(byText("Male")).click();
+//        $("#currentAddress").setValue("Some address 1");
+//        $("#permanentAddress").setValue("Another address 1");
+//        $("#submit").click();
+
     }
 }
